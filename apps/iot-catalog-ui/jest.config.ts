940fa7278ff3cab530be451f9130402dd16b89cd
David <david.lfelguera@nicepeopleatwork.com>
2022-08-13 00:03:08 +0200

    initial commit

---
+++ apps/iot-catalog-ui/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'iot-catalog-ui',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/iot-catalog-ui',
+};
---
+++ apps/iot-catalog-ui/src/app/app.tsx
@@ -0,0 +1,14 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <style jsx>{`
+        /** your style here **/
+      `}</style>
+      <NxWelcome title="iot-catalog-ui" />
+    </>
+  );
+}
+
+export default App;
